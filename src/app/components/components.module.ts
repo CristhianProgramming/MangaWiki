3edf524b9fbdd55b550d2ff61fc58e0a93a19b87
cristhian <m11mario11m@gmail.com>
2023-05-11 10:55:34 -0500

    configure routing

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RecomendationsComponent } from './pages/recomendations/recomendations.component';
 import { CardsComponent } from './pages/cards/cards.component';
 import { TitlepipePipe } from './pipes/titlepipe.pipe';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { ComponentRoutingModule } from './components-routing.modules';
 
 
 
@@ -10,11 +15,16 @@ import { TitlepipePipe } from './pipes/titlepipe.pipe';
   declarations: [
     RecomendationsComponent,
     CardsComponent,
-    TitlepipePipe
+    TitlepipePipe,
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterModule,
+    ComponentRoutingModule
   ],
-  exports:[RecomendationsComponent]
+  exports:[RecomendationsComponent,HomeComponent,DetailsComponent]
 })
 export class ComponentsModule { }
