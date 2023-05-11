3606fa268a394b386078f978f99a8f744574a07b
cristhian <m11mario11m@gmail.com>
2023-05-11 10:09:13 -0500

    finish principal web site design

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RecomendationsComponent } from './recomendations/recomendations.component';
+import { RecomendationsComponent } from './pages/recomendations/recomendations.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { TitlepipePipe } from './pipes/titlepipe.pipe';
 
 
 
 @NgModule({
   declarations: [
-    RecomendationsComponent
+    RecomendationsComponent,
+    CardsComponent,
+    TitlepipePipe
   ],
   imports: [
     CommonModule
---
+++ src/app/components/pages/recomendations/recomendations.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { KitsuService } from '../../service/kitsu.service';
+import { Datum } from '../../interfaces/Kitsu.interface';
+
+@Component({
+  selector: 'app-recomendations',
+  templateUrl: './recomendations.component.html',
+  styleUrls: ['./recomendations.component.css']
+})
+export class RecomendationsComponent implements OnInit{
+
+  public isPrimary? : string | null ;
+
+  public tenRecomendacion : Datum[] = [] ;
+
+  constructor(private serviceKitsu : KitsuService){}
+
+  @Input() propertyes : string[] | undefined;
+
+  ngOnInit(): void {
+    this.isPrimary = this.propertyes![1] ;
+   this.serviceKitsu.getContent(this.propertyes![0]).subscribe(response => { 
+    response.data.map(contenido =>{
+      this.tenRecomendacion.push(contenido)
+    })
+  })
+  }
+}
